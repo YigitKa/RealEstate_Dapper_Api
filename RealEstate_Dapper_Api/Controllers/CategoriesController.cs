1dc45ff05f0f6ea1a8ba89c76821709525c419fa
Yiğit KATIRCIOĞLU <90yigit@gmail.com>
2024-01-27 18:05:40 +0300

    örnek dosyalar silindi, di eklendi
    proje olıuşturulurken gelen örnek dosyalar silindi,
    Vt bağlantısına sunucuya güvenme ayarı eklendi,
    Kategoriler listelenmesi için API'de sınıf eklendi

---
+++ RealEstate_Dapper_Api/Controllers/CategoriesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Repositories.CategoryRepository;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoriesController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CategoryList()
+        {
+            var values = await _categoryRepository.GetAllCategoryAsync();
+            return Ok(values);
+        }
+    }
+}
+
