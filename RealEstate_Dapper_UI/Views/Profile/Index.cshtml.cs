f868e30ea295310bb5c89cbd98d6e95e18b761c0
Yiğit Katırcıoğlu <yigit.katircioglu@outlook.com>
2024-01-27 17:11:03 +0300

    first commit for initial

---
+++ RealEstate_Dapper_UI/Views/Profile/Index.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RealEstate_Dapper_UI.Views.Profile
+{
+	public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
