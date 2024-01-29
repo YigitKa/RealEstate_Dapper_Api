95e8f80b88d69854803b6e624337255670401d2f
Yiğit KATIRCIOĞLU <90yigit@gmail.com>
2024-01-29 23:16:09 +0300

    Admin tarafı componentleri ayarlanmaya başlandı.
    Kategoriler listelendi

---
+++ RealEstate_Dapper_UI/ViewComponents/AdminLayout/_AdminLayoutSideBarComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Dapper_UI.ViewComponents.AdminLayout
+{
+	public class _AdminLayoutSideBarComponentPartial : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
+
