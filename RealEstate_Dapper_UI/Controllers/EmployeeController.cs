3563a37bdc04d08024049621d8588c358fd443db
Yiğit KATIRCIOĞLU <90yigit@gmail.com>
2024-02-05 21:11:23 +0300

    Employee UI listeleme yapıldı
    DTOlar UI'a eklendi. Employee UI listelemesi yapıldı

---
+++ RealEstate_Dapper_UI/Controllers/EmployeeController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.DTOs.EmployeeDTOs;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RealEstate_Dapper_UI.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public EmployeeController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7123/api/Employees");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultEmployeeDTO>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateEmployee(CreateEmployeeDTO createEmployeeDTO)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createEmployeeDTO);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7123/api/Employees", stringContent);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var reponseMessage = await client.DeleteAsync($"https://localhost:7123/api/Employees/{id}");
+            if (reponseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateEmployee(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7123/api/Employees/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateEmployeeDTO>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateEmployee(UpdateEmployeeDTO updateEmployeeDTO)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateEmployeeDTO);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7123/api/Employees/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
+
---
+++ RealEstate_Dapper_UI/DTOs/EmployeeDTOs/DeleteEmployeeDTO.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace RealEstate_Dapper_UI.DTOs.EmployeeDTOs
+{
+    public class DeleteEmployeeDTO
+    {
+        public int EmployeeID { get; set; }
+    }
+}
+
