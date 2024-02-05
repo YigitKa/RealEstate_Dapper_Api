2889dad0500ac21175c3db8b7f4df1dabc8600ba
Yiğit KATIRCIOĞLU <90yigit@gmail.com>
2024-02-05 21:01:15 +0300

    Employee contoller güncellendi
    EmployeeController fonkisyonları eklendi.

--- RealEstate_Dapper_Api/Controllers/EmployeesController.cs
+++ RealEstate_Dapper_Api/Controllers/EmployeesController.cs
@@ -3,18 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.DTOs.EmployeeDTOs;
+using RealEstate_Dapper_Api.Repositories.EmployeeRepositories;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace RealEstate_Dapper_Api.Controllers
 {
     [Route("api/[controller]")]
-    public class EmployeesController : Controller
+    public class EmployeesController : ControllerBase
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeesController(IEmployeeRepository employeeRepository)
         {
-            return View();
+            _employeeRepository = employeeRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EmployeeList()
+        {
+            var values = await _employeeRepository.GetAllEmployeeAsync();
+            return Ok(values);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateEmployee([FromBody] CreateEmployeeDTO createEmployeeDTO)
+        {
+            _employeeRepository.CreateEmployee(createEmployeeDTO);
+            return Ok("Personel Başarılı Bir Şekilde Eklendi");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee([FromBody] DeleteEmployeeDTO deleteEmployeeDTO)
+        {
+            _employeeRepository.DeleteEmployee(deleteEmployeeDTO);
+            return Ok("Personel silindi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateEmployee([FromBody] UpdateEmployeeDTO updateEmployeeDTO)
+        {
+            _employeeRepository.UpdateEmployee(updateEmployeeDTO);
+            return Ok("Personel güncellendi");
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEmployee(int id)
+        {
+            var value = await _employeeRepository.GetEmployee(id);
+            return Ok(value);
         }
     }
 }
--- RealEstate_Dapper_Api/Program.cs
+++ RealEstate_Dapper_Api/Program.cs
@@ -1,6 +1,7 @@
 ﻿using RealEstate_Dapper_Api.Models.DapperContext;
 using RealEstate_Dapper_Api.Repositories.BottomGridRepository;
 using RealEstate_Dapper_Api.Repositories.CategoryRepository;
+using RealEstate_Dapper_Api.Repositories.EmployeeRepositories;
 using RealEstate_Dapper_Api.Repositories.PopularLocationRepository;
 using RealEstate_Dapper_Api.Repositories.ProductRepository;
 using RealEstate_Dapper_Api.Repositories.ServiceRepository;
@@ -16,6 +17,7 @@ builder.Services.AddTransient<IWhoWeAreDetailRepository, WhoWeAreDetailRepositor
 builder.Services.AddTransient<IServiceRepository, ServiceRepository>();
 builder.Services.AddTransient<IBottomGridRepository, BottomGridRepository>();
 builder.Services.AddTransient<IPopularLocationRepository, PopularLocationRepository>();
+builder.Services.AddTransient<IEmployeeRepository, EmployeeRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
